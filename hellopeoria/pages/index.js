05b7677256dff226aab1e814b1e9f186bfe0ed98
Daniel Hart <me@danielhart.co>
2018-11-20 08:33:02 -0600

    Added dummy content to homepage

--- hellopeoria/pages/index.js
+++ hellopeoria/pages/index.js
@@ -1,9 +1,37 @@
 const Home = props => (
     <div>
      	<h1>Homepage</h1>
-       	<h2>This is an h2</h2>
-        <p>Hello!</p>
-        <button>This is a button</button>
+        <div className="card-wrapper">
+            <div className="category card coffee">
+                <div className="inner">
+                    <h2>Coffee</h2>
+                    <p>Java to go, or stay</p>
+                </div>
+            </div>
+
+            <div className="category card restaurant">
+                <div className="inner">
+                    <h2>Restaurants</h2>
+                    <p>Grab a bite</p>
+                </div>
+            </div>
+
+            <div className="category card event">
+                <div className="inner">
+                    <h2>Events</h2>
+                    <p>Things to do around town</p>
+                </div>
+            </div>
+
+            <div className="category card breakfast">
+                <div className="inner">
+                    <h2>Breakfast</h2>
+                    <p>For the early riser</p>
+                </div>
+            </div>
+        </div>
+       	
+        <div align="center"><button>More</button></div>
     </div>
 )
 
