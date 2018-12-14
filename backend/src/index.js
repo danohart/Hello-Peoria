4b5006729b30a7fd778fe35eb34a526069d6788b
Daniel Hart <me@danielhart.co>
2018-12-14 16:29:03 -0600

    Temp disable

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,12 +8,12 @@ const server = createServer();
 // TODO Use express middle to populate current user
 
 server.start(
-    {
-        cors: {
-            credentials: true,
-            origin: process.env.FRONTEND_URL,
-        },
-    }, 
+    // {
+    //     cors: {
+    //         credentials: true,
+    //         origin: process.env.FRONTEND_URL,
+    //     },
+    // }, 
     deets => {
         console.log(`Server is now running on http://localhost:${deets.port}`);
     }
