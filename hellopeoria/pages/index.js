2884b5312d1578fc4ae73e07fd9fcae792cce5b6
Daniel Hart <me@danielhart.co>
2018-12-14 15:35:53 -0600

    Link homepage buttons to Places route

--- hellopeoria/pages/index.js
+++ hellopeoria/pages/index.js
@@ -4,31 +4,39 @@ const Home = props => (
     <div>
         <div className="card-wrapper">
             <div className="category card coffee">
+            <Link href='/places'><a>
                 <div className="inner">
                     <h2>Coffee</h2>
                     <p>Java to go, or stay</p>
                 </div>
+            </a></Link>
             </div>
 
             <div className="category card restaurant">
+            <Link href='/places'><a>
                 <div className="inner">
                     <h2>Restaurants</h2>
                     <p>Grab a bite</p>
                 </div>
+            </a></Link>
             </div>
 
             <div className="category card event">
+            <Link href='/places'><a>
                 <div className="inner">
                     <h2>Events</h2>
                     <p>Things to do around town</p>
                 </div>
+            </a></Link>
             </div>
 
             <div className="category card breakfast">
+            <Link href='/places'><a>
                 <div className="inner">
                     <h2>Breakfast</h2>
                     <p>For the early riser</p>
                 </div>
+            </a></Link>
             </div>
         </div>
        	
