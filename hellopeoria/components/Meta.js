0254e70b6c22be639f75090d2a11a86f8136cc7c
Daniel Hart <me@danielhart.co>
2019-01-02 23:52:39 -0600

    Remove react-ga

--- hellopeoria/components/Meta.js
+++ hellopeoria/components/Meta.js
@@ -1,8 +1,4 @@
 import Head from 'next/head';
-import ReactGA from 'react-ga';
-
-ReactGA.initialize('UA-41524322-16');
-ReactGA.pageview(window.location.pathname + window.location.search);
 
 const Meta = () => (
     <Head>
