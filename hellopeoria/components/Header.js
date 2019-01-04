2d536b512ec62c5f24fe91f7ace0aa807de94ad6
Daniel Hart <me@danielhart.co>
2019-01-03 22:00:23 -0600

    Add progress bar

--- hellopeoria/components/Header.js
+++ hellopeoria/components/Header.js
@@ -1,7 +1,19 @@
 import Nav from './Nav';
 import Search from './Search';
 import Link from 'next/link';
+import NProgress from 'nprogress';
 import Router from 'next/router';
+
+Router.onRouteChangeStart = () => {
+    NProgress.start();
+  };
+  Router.onRouteChangeComplete = () => {
+    NProgress.done();
+  };
+  
+  Router.onRouteChangeError = () => {
+    NProgress.done();
+  };
 
 const Header = () => (
     <header>
