e2c9fa9bdd03085ed62c31ea00c682e6b4273b01
Daniel Hart <me@danielhart.co>
2019-04-07 14:50:48 -0500

    Update links on homepage to link to bar and coffee pages

--- hellopeoria/pages/index.js
+++ hellopeoria/pages/index.js
@@ -1,47 +1,59 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 const Home = props => (
-    <div>
-        <div className="card-wrapper">
-            <div className="category card coffee">
-            <Link href='/places'><a>
-                <div className="inner">
-                    <h2>Coffee</h2>
-                    <p>Java to go, or stay</p>
-                </div>
-            </a></Link>
+  <div>
+    <div className="card-wrapper">
+      <div className="category card coffee">
+        <Link href="/coffee-places">
+          <a>
+            <div className="inner">
+              <h2>Coffee</h2>
+              <p>Java to go, or stay</p>
             </div>
+          </a>
+        </Link>
+      </div>
 
-            <div className="category card restaurant">
-            <Link href='/places'><a>
-                <div className="inner">
-                    <h2>Restaurants</h2>
-                    <p>Grab a bite</p>
-                </div>
-            </a></Link>
+      <div className="category card restaurant">
+        <Link href="/places">
+          <a>
+            <div className="inner">
+              <h2>Restaurants</h2>
+              <p>Grab a bite</p>
             </div>
+          </a>
+        </Link>
+      </div>
 
-            <div className="category card event">
-            <Link href='/places'><a>
-                <div className="inner">
-                    <h2>Events</h2>
-                    <p>Things to do around town</p>
-                </div>
-            </a></Link>
+      <div className="category card bar">
+        <Link href="/bar-places">
+          <a>
+            <div className="inner">
+              <h2>Bars</h2>
+              <p>Chill places for a drink</p>
             </div>
+          </a>
+        </Link>
+      </div>
 
-            <div className="category card breakfast">
-            <Link href='/places'><a>
-                <div className="inner">
-                    <h2>Breakfast</h2>
-                    <p>For the early riser</p>
-                </div>
-            </a></Link>
+      <div className="category card breakfast">
+        <Link href="/places">
+          <a>
+            <div className="inner">
+              <h2>Breakfast</h2>
+              <p>For the early riser</p>
             </div>
-        </div>
-       	
-        <div align="center"><Link href="/places"><button>More</button></Link></div>
+          </a>
+        </Link>
+      </div>
     </div>
-)
+
+    <div align="center">
+      <Link href="/places">
+        <button>More</button>
+      </Link>
+    </div>
+  </div>
+);
 
 export default Home;
