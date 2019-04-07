10094293cccbd34fe31278627bdd0ee9604b29dc
Daniel Hart <me@danielhart.co>
2019-04-07 15:26:50 -0500

    Add analytics component to page layout

--- hellopeoria/components/Page.js
+++ hellopeoria/components/Page.js
@@ -1,23 +1,28 @@
-import React, { Component } from 'react';
-import Header from './Header';
-import HomeHeader from './HomeHeader';
-import Meta from './Meta';
+import React, { Component } from "react";
+import { initGA, logPageView } from "../lib/analytics";
+import Header from "./Header";
+import Meta from "./Meta";
 
 // Styles
-import '../styles/style.scss';
+import "../styles/style.scss";
 
 class Page extends Component {
-    render() {
-        return (
-            <div>
-                <Meta />
-                <Header />
-                <div className="main">
-                	{this.props.children}
-                </div>
-            </div>
-        );
+  componentDidMount() {
+    if (!window.GA_INITIALIZED) {
+      initGA();
+      window.GA_INITIALIZED = true;
     }
+    logPageView();
+  }
+  render() {
+    return (
+      <div>
+        <Meta />
+        <Header />
+        <div className="main">{this.props.children}</div>
+      </div>
+    );
+  }
 }
 
 export default Page;
