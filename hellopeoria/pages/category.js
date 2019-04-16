815b606e692e9d3473613fa8e339bcdeefceecdd
Daniel Hart <me@danielhart.co>
2019-04-16 14:09:38 -0400

    Add title to caregory page lists

--- hellopeoria/pages/category.js
+++ hellopeoria/pages/category.js
@@ -2,6 +2,7 @@ import CategoryPlaces from '../components/Category';
 
 const Category = props => (
   <div>
+    <h1>{props.query.category}s</h1>
     <CategoryPlaces category={props.query.category} />
   </div>
 );
