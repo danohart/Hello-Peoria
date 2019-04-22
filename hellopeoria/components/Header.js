a026829bb9edda02ce9e3043b2fb944fe2bcaed1
Daniel Hart <me@danielhart.co>
2019-04-22 16:00:43 -0500

    Move Meta to homepage

--- hellopeoria/components/Header.js
+++ hellopeoria/components/Header.js
@@ -18,17 +18,6 @@ Router.onRouteChangeError = () => {
 
 const Header = () => (
   <header>
-    <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta charSet="utf-8" />
-      <link rel="stylesheet" type="text/css" href="static/nprogress.css" />
-      <link rel="shortcut icon" href="/static/favicon.png" />
-      <title>Hello Peoria // What to do in Peoria, IL right now</title>
-      <meta
-        name="description"
-        content="Restaurants, Bars, and Events in Peoria. Find out cool things to see and do in Peoria, IL"
-      />
-    </Head>
     <div className="logo">
       <Link href={{ pathname: '/' }}>
         <a>
--- hellopeoria/pages/index.js
+++ hellopeoria/pages/index.js
@@ -1,7 +1,19 @@
 import Link from 'next/link';
+import Head from 'next/head';
 
 const Home = props => (
   <div>
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta charSet="utf-8" />
+      <link rel="stylesheet" type="text/css" href="static/nprogress.css" />
+      <link rel="shortcut icon" href="/static/favicon.png" />
+      <title>Hello Peoria // What to do in Peoria, IL right now</title>
+      <meta
+        name="description"
+        content="Restaurants, Bars, and Events in Peoria. Find out cool things to see and do in Peoria, IL"
+      />
+    </Head>
     <div className="card-wrapper">
       <div className="category card coffee">
         <Link href="/category?category=Coffee">
