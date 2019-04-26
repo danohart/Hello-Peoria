b94fdf51b6ffe0a5bc45f29043a632e09c9338d6
Daniel Hart <me@danielhart.co>
2019-04-26 11:30:16 -0500

    Update meta tags from category and path pages

--- hellopeoria/pages/category.js
+++ hellopeoria/pages/category.js
@@ -1,7 +1,18 @@
 import CategoryPlaces from '../components/Category';
+import Head from 'next/head';
 
 const Category = props => (
   <div>
+    <Head>
+      <title>
+        {props.query.category} - Hello Peoria // What to do in Peoria, IL right
+        now
+      </title>
+      <meta
+        name="description"
+        content={props.query.category + 'in Peoria, IL'}
+      />
+    </Head>
     <h1 className="page-header">{props.query.category}s</h1>
     <CategoryPlaces category={props.query.category} />
   </div>
--- hellopeoria/pages/path.js
+++ hellopeoria/pages/path.js
@@ -1,7 +1,12 @@
 import PathPlaces from '../components/Path';
+import Head from 'next/head';
 
 const Path = props => (
   <div>
+    <Head>
+      <title>{props.query.paths} - Hello Peoria // Places in Peoria, IL</title>
+      <meta name="description" content={props.query.paths + 'in Peoria, IL'} />
+    </Head>
     <h1 className="page-header">{props.query.paths}</h1>
     <PathPlaces paths={props.query.paths} />
   </div>
