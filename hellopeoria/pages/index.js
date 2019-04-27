faef3ddf22acf5d50af7daf749741912618d4a60
Daniel Hart <me@danielhart.co>
2019-04-27 12:49:36 -0500

    Change homepage into stateful component

--- hellopeoria/pages/index.js
+++ hellopeoria/pages/index.js
@@ -1,99 +1,104 @@
 import Link from 'next/link';
-import Head from 'next/head';
 
-const Home = props => (
-  <div>
-    <div className="card-wrapper">
-      <div className="category card coffee">
-        <Link href="/category?category=Coffee">
-          <a>
-            <div className="inner">
-              <h2>Coffee</h2>
-              <p>Java to go, or stay</p>
-            </div>
-          </a>
-        </Link>
-      </div>
+import React, { Component } from 'react';
 
-      <div className="category card restaurant">
-        <Link href="/category?category=Restaurant">
-          <a>
-            <div className="inner">
-              <h2>Restaurants</h2>
-              <p>Grab a bite</p>
-            </div>
-          </a>
-        </Link>
-      </div>
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <div className="card-wrapper">
+          <div className="category card coffee">
+            <Link href="/category?category=Coffee">
+              <a>
+                <div className="inner">
+                  <h2>Coffee</h2>
+                  <p>Java to go, or stay</p>
+                </div>
+              </a>
+            </Link>
+          </div>
 
-      <div className="category card bar">
-        <Link href="/category?category=Bar">
-          <a>
-            <div className="inner">
-              <h2>Bars</h2>
-              <p>Chill places for a drink</p>
-            </div>
-          </a>
-        </Link>
-      </div>
+          <div className="category card restaurant">
+            <Link href="/category?category=Restaurant">
+              <a>
+                <div className="inner">
+                  <h2>Restaurants</h2>
+                  <p>Grab a bite</p>
+                </div>
+              </a>
+            </Link>
+          </div>
 
-      <div className="category card breakfast">
-        <Link href="/breakfast-places">
-          <a>
-            <div className="inner">
-              <h2>Breakfast</h2>
-              <p>For the early riser</p>
-            </div>
-          </a>
-        </Link>
-      </div>
-    </div>
+          <div className="category card bar">
+            <Link href="/category?category=Bar">
+              <a>
+                <div className="inner">
+                  <h2>Bars</h2>
+                  <p>Chill places for a drink</p>
+                </div>
+              </a>
+            </Link>
+          </div>
 
-    <h2 align="center">Pick Your Path</h2>
+          <div className="category card breakfast">
+            <Link href="/breakfast-places">
+              <a>
+                <div className="inner">
+                  <h2>Breakfast</h2>
+                  <p>For the early riser</p>
+                </div>
+              </a>
+            </Link>
+          </div>
+        </div>
 
-    <div className="paths-wrapper">
-      <div className="path free">
-        <Link href="/path?paths=Free">
-          <a>Free</a>
-        </Link>
-      </div>
-      <div className="path family">
-        <Link href="/path?paths=Family">
-          <a>Family</a>
-        </Link>
-      </div>
-      {/* <div className="path sightseeing">
+        <h2 align="center">Pick Your Path</h2>
+
+        <div className="paths-wrapper">
+          <div className="path free">
+            <Link href="/path?paths=Free">
+              <a>Free</a>
+            </Link>
+          </div>
+          <div className="path family">
+            <Link href="/path?paths=Family">
+              <a>Family</a>
+            </Link>
+          </div>
+          {/* <div className="path sightseeing">
         <Link href="/path?paths=Sightseeing">
           <a>Sightseeing</a>
         </Link>
       </div> */}
-      <div className="path nightlife">
-        <Link href="/path?paths=Nightlife">
-          <a>Night Life</a>
-        </Link>
-      </div>
-      <div className="path local">
-        <Link href="/path?paths=Local">
-          <a>Local</a>
-        </Link>
-      </div>
-      <div className="path foodie">
-        <Link href="/path?paths=Foodie">
-          <a>Foodie</a>
-        </Link>
-      </div>
-      {/* <div className="path outdoor">
+          <div className="path nightlife">
+            <Link href="/path?paths=Nightlife">
+              <a>Night Life</a>
+            </Link>
+          </div>
+          <div className="path local">
+            <Link href="/path?paths=Local">
+              <a>Local</a>
+            </Link>
+          </div>
+          <div className="path foodie">
+            <Link href="/path?paths=Foodie">
+              <a>Foodie</a>
+            </Link>
+          </div>
+          {/* <div className="path outdoor">
         <Link href="/path?paths=Outdoor">
           <a>Outdoor</a>
         </Link>
       </div> */}
-      {/* <div className="path events">
+          {/* <div className="path events">
         <Link href="/path?paths=Events">
           <a>Events</a>
         </Link>
       </div> */}
-    </div>
-  </div>
-);
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Home;
