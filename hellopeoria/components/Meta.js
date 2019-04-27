bff05e192fa08e2000b370fe7f191eb068a6f44a
Daniel Hart <me@danielhart.co>
2019-04-27 12:13:25 -0500

    Update overall main meta tags

--- hellopeoria/components/Meta.js
+++ hellopeoria/components/Meta.js
@@ -8,9 +8,37 @@ const Meta = props => (
     <link rel="shortcut icon" href="/static/favicon.png" />
     <title>Hello Peoria // What to do in Peoria, IL right now</title>
     <meta
+      itemProp="description"
+      content="Restaurants, Bars, and Events in Peoria. Find out cool things to see and do in Peoria, IL"
+    />
+    <meta itemProp="image" content="/static/images/logo.png" />
+    <meta
       name="description"
       content="Restaurants, Bars, and Events in Peoria. Find out cool things to see and do in Peoria, IL"
     />
+    {/*<!-- Facebook Meta Tags -->*/}
+    <meta
+      property="og:title"
+      content="Hello Peoria // What to do in Peoria, IL right now"
+    />
+    <meta
+      property="og:description"
+      content="Restaurants, Bars, and Events in Peoria. Find out cool things to see and do in Peoria, IL"
+    />
+    <meta property="og:image" content="/static/images/logo.png" />
+    <meta property="og:type" content="website" />
+
+    {/*<!-- Twitter Meta Tags -->*/}
+    <meta
+      name="twitter:title"
+      content="Hello Peoria // What to do in Peoria, IL right now"
+    />
+    <meta
+      name="twitter:description"
+      content="Restaurants, Bars, and Events in Peoria. Find out cool things to see and do in Peoria, IL"
+    />
+    <meta name="twitter:image" content="/static/images/logo.png" />
+    <meta name="twitter:card" content="summary_large_image" />
   </Head>
 );
 
