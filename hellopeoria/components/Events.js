c145115344fc2d990661d6b0dcaa65663b676b0a
Daniel Hart <me@danielhart.co>
2019-08-05 18:26:55 -0500

    Update Event query

--- hellopeoria/components/Events.js
+++ hellopeoria/components/Events.js
@@ -18,57 +18,62 @@ const ALL_EVENTS_QUERY = gql`
 class Events extends Component {
   render() {
     return (
-        <Query query={ALL_EVENTS_QUERY}>
-          {({ data, error, loading }) => {
-              if (loading) return <Loading />;
-              if (error) return <p>Error: {error.message}</p>;
-              console.log(data);
-              return (
-                <div className="card-wrapper">
-                   <Head>
-                    <title>Events happening in Peoria, IL // Hello Peoria</title>
-                    <meta name="description" content="What to do in Peoria today and this month. Events in Peoria, IL" />
-                  </Head>
-                  {data.event.data.map(event => (
-                    <div className="card" key={event.id}>
-                    <div className="date">
-                      <h3>
-                        <Moment format="MMMM, D - ha">{event.start_time}</Moment>
-                      </h3>
-                    </div>
-                    <div
-                      className="image"
-                      style={{ backgroundImage: `url(${event.cover.source})` }}
-                    >
-                      <Link href={`https://facebook.com/event/` + event.id}>
-                        <a target="_blank" className="featured-link" />
-                      </Link>
-        
-                      <div className="title-wrapper">
-                        <div className="place-category">
-                          <span>
-                            <Link href={`https://facebook.com/event/` + event.id}>
-                              <a>{event.place.name}</a>
-                            </Link>
-                          </span>
-                        </div>
-                        <div className="title">
-                          <h2>{event.name}</h2>
-                        </div>
+      <Query query={ALL_EVENTS_QUERY}>
+        {({ data, error, loading }) => {
+          if (loading) return <Loading />;
+          if (error) return <p>Error: {error.message}</p>;
+          console.log(data);
+          return (
+            <div className="card-wrapper">
+              <Head>
+                <title>Events happening in Peoria, IL // Hello Peoria</title>
+                <meta
+                  name="description"
+                  content="What to do in Peoria today and this month. Events in Peoria, IL"
+                />
+              </Head>
+              {data.event.data.map(event => (
+                <div className="card" key={event.id}>
+                  <div className="date">
+                    <h3>
+                      <Moment format="MMMM, D - ha">{event.start_time}</Moment>
+                    </h3>
+                  </div>
+                  <div
+                    className="image"
+                    style={{
+                      backgroundImage: `url(${event.cover.source})`,
+                    }}
+                  >
+                    <Link href={`https://facebook.com/event/` + event.id}>
+                      <a target="_blank" className="featured-link" />
+                    </Link>
+
+                    <div className="title-wrapper">
+                      <div className="place-category">
+                        <span>
+                          <Link href={`https://facebook.com/event/` + event.id}>
+                            <a>{event.place.name}</a>
+                          </Link>
+                        </span>
+                      </div>
+                      <div className="title">
+                        <h2>{event.name}</h2>
                       </div>
                     </div>
-                    <div className="description">
-                      {event.description.substring(0, 180)}...
-                    </div>
-                    <Link href={`https://facebook.com/event/` + event.id}>
-                      <a className="button full-size">Learn More</a>
-                    </Link>
                   </div>
-                  ))}
+                  <div className="description">
+                    {event.description.substring(0, 180)}...
+                  </div>
+                  <Link href={`https://facebook.com/event/` + event.id}>
+                    <a className="button full-size">Learn More</a>
+                  </Link>
                 </div>
-              );
-            }}
-        </Query>
+              ))}
+            </div>
+          );
+        }}
+      </Query>
     );
   }
 }
