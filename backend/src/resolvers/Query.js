5b80891f91f52219edfd718d8c5ba59a301db2cc
Daniel Hart <me@danielhart.co>
2019-08-07 10:22:50 -0500

    Set up Query resolver for Foursquare search data

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -3,11 +3,17 @@ const { hasPermission } = require('../utils');
 require('dotenv');
 const fetch = require('node-fetch');
 
+// Facebook Graph API constants
 const FB_FIELDS = 'cover,name,place,description,start_time';
 const FB_API =
   `https://graph.facebook.com/v4.0/1353765521380739/events?access_token=` +
   process.env.FACEBOOK_ACCESS_TOKEN +
   `&fields=${FB_FIELDS}`;
+
+// Foursquare Places API constants
+const FSQ_ID = process.env.FSQ_ID;
+const FSQ_SECRET = process.env.FSQ_SECRET;
+const FSQ_API = `https://api.foursquare.com/v2/venues/search?client_id=${FSQ_ID}&client_secret=${FSQ_SECRET}&query=coffee`;
 
 const Query = {
   places: forwardTo('db'),
@@ -19,7 +25,7 @@ const Query = {
     }
     return ctx.db.query.user(
       {
-        where: { id: ctx.request.userId },
+        where: { id: ctx.request.userId }
       },
       info
     );
@@ -35,13 +41,21 @@ const Query = {
     //3. If they do, query all users
     return ctx.db.query.users({}, info);
   },
-
+  // Facebook Graph API for events
   async event(parent, args) {
     // const { id } = args
     const response = await fetch(FB_API);
     const fbEvent = await response.json();
     return fbEvent;
   },
+  // Foursquare Places API
+
+  async fsqInfo(parent, args) {
+    // const { id } = args
+    const response = await fetch(FSQ_API);
+    const fsqInfo = await response.json();
+    return fsqInfo;
+  }
 };
 
 module.exports = Query;
