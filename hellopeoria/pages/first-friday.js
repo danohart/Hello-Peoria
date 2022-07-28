68dc7ae3c9b1b0ffc7e1021a21179892227f592f
Daniel Hart <me@danielhart.co>
2022-07-28 13:41:02 -0500

    Add copy to First Friday page

--- hellopeoria/pages/first-friday.js
+++ hellopeoria/pages/first-friday.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import { useQuery, gql } from '@apollo/client';
-import Place from '../components/Place';
-import Error from '../components/ErrorMessage';
-import Loading from '../components/Loading';
-import { perPage } from '../config';
+import React from "react";
+import { useQuery, gql } from "@apollo/client";
+import Place from "../components/Place";
+import Error from "../components/ErrorMessage";
+import Head from "next/head";
+import Loading from "../components/Loading";
+import { perPage } from "../config";
 
 const ALL_PLACES_QUERY = gql`
   query {
@@ -33,10 +34,40 @@ export default function FirstFriday() {
   if (error) return <Error error={error} />;
 
   return (
-    <div className='card-wrapper'>
-      {data.allPeoriaPlaces.map((place) => (
-        <Place place={place} key={place.id} />
-      ))}
-    </div>
+    <>
+      <Head>
+        <title>
+          Peoria's First Friday | Art Gallery Openings // Hello Peoria
+        </title>
+        <meta
+          name='description'
+          content="What's Happening in Peoria on First Fridays. First Friday is a national movement among artists in many cities across the United States."
+        />
+        <meta
+          itemProp='image'
+          content='http://hellopeoria.co/static/images/logo.png'
+        />
+      </Head>
+      <h1>Peoria First Fridays</h1>
+      <div className='advert alt'>
+        <div className='ad-inner'>
+          <p>
+            First Friday is a national movement among artists in many cities
+            across the United States. Artists open their studios to present
+            their work to the public.
+          </p>
+
+          <p>
+            First Friday events keep the arts in the forefront of a community.
+          </p>
+        </div>
+      </div>
+
+      <div className='card-wrapper'>
+        {data.allPeoriaPlaces.map((place) => (
+          <Place place={place} key={place.id} />
+        ))}
+      </div>
+    </>
   );
 }
